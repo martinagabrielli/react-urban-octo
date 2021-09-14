a7d22f08e42a608ad435456ba6f6bf11d69db9dc
Martina <martina.gabrielli@yahoo.it>
2021-09-14 19:01:38 +0100

    Add latest changes

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Counter from './Counter';
 
-class Counters extends React.Component {
+class Counters extends Component {
     state = {
         counters: [
             { id: 1, value: 4 },
@@ -14,18 +14,18 @@ class Counters extends React.Component {
     handleDelete = counterId => {
         console.log(counterId)
         const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters})
+        this.setState({ counters })
     }
 
-    render() { 
+    render() {
         return (
             <div>
                 {this.state.counters.map(counter => (
-                <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} />
+                    <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} />
                 ))}
             </div>
         )
     }
 }
- 
+
 export default Counters;
