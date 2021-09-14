9d79a182db0ce3df39b06ca705da42859f4cbc35
Martina <martina.gabrielli@yahoo.it>
2021-09-14 19:29:10 +0100

    Change way of passing props

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        value: this.props.value
+        value: this.props.counter.value
     };
 
     handleIncrement = (product) => {
@@ -18,7 +18,7 @@ class Counter extends Component {
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-            <button onClick={() => this.props.onDelete(this.props.id)} className="btn-delete btn-sm m-2">Delete</button>
+            <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn-delete btn-sm m-2">Delete</button>
         </div>
         );
     }
