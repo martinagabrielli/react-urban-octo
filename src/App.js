b6a5e3e78bc97bb8a2c6a9c67bf28a214e875393
martinagabrielli <martina.gabrielli@yahoo.it>
2021-09-18 15:56:48 +0100

    Change files after Navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
+import Counters from './components/Counters';
+import Counter from './components/Counter';
+import { Fragment } from 'react/cjs/react.production.min';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters />
+      </main>
+    </Fragment>
   );
 }
 
