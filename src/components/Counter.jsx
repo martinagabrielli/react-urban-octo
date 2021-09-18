2d1d615dbc00ae6f61db712366ec32a73c2836be
martinagabrielli <martina.gabrielli@yahoo.it>
2021-09-18 15:42:17 +0100

    Removed local state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,11 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value
-    };
-
-    handleIncrement = (product) => {
-        this.setState({value: this.state.value + 1});
-    }
-
-    doHandleIncrement = () => {
-        this.handleIncrement({id: 1});
-    }
-
     render() {
         return ( 
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
             <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn-delete btn-sm m-2">Delete</button>
         </div>
         );
@@ -25,12 +13,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? 'Zero' : value;
     }
 }
